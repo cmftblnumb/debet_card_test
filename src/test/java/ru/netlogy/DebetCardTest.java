706ccb40e639b7a9503b29eefc640bd5226182c8
Kristina P <cmftblnumb@gmail.com>
2025-02-12 09:35:09 -0500

    renamed

--- src/test/java/ru/netlogy/DebetCardTest.java
+++ src/test/java/ru/netlogy/DebetCardTest.java
@@ -8,11 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
